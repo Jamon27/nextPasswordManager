6d1f04ee45eb45c961e1bcc0e6964cc23e7da818
Alexey M <moiseev27@gmail.com>
2023-01-02 21:10:55 +0530

    Change pages' head

--- next-password-manager/pages/signup.tsx
+++ next-password-manager/pages/signup.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Регистрация</title>
         <meta name="description" content="Simple password manager" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon1.ico" />
