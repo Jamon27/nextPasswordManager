8f8104661e83e78a9c817c66e89e41025f4c467b
Alexey M <moiseev27@gmail.com>
2023-01-02 20:13:42 +0530

    Add signup layout

---
+++ next-password-manager/pages/signup.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Simple password manager" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon1.ico" />
+      </Head>
+      <main className={styles.main}>
+        <form>
+          <div className='container'>
+            <p>Для регистрации укажите ваше имя, логин и пароль.</p>
+            <label htmlFor="name"><b>Имя</b></label>
+            <input type="text" placeholder="Введите имя" name="name" required />
+            <label htmlFor="login"><b>Логин</b></label>
+            <input type="text" placeholder="Введите логин" name="login" required />
+            <label htmlFor="password"><b>Пароль</b></label>
+            <input type="password" placeholder="Введите пароль" name="password" required />
+            <label htmlFor="password"><b>Повторите пароль</b></label>
+            <input type="password" placeholder="Повторите пароль" name="repeat-password" required />
+            <button type="submit">Вход</button>
+          </div>
+        </form>
+      </main>
+    </>
+  )
+}
